d199c10b80f4bf3711a5571bf680cd3b27605ac9
Ali Kurtulmus <mkurtulmus.eng@gmail.com>
2022-08-18 11:19:37 +0300

    Add project files.

---
+++ csharp_dosya-islemleri_my/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace csharp_dosya_islemleri_my
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnyolsec = new System.Windows.Forms.Button();
+            this.btnolustur = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 43);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open File Dialog";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(162, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(130, 43);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Save File Dialog";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(310, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(130, 43);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Folder Browser Dialog";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Dosya Yolu:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 152);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Dosya Adı:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(126, 95);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(317, 22);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(126, 149);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(317, 22);
+            this.textBox2.TabIndex = 6;
+            // 
+            // btnyolsec
+            // 
+            this.btnyolsec.Location = new System.Drawing.Point(466, 91);
+            this.btnyolsec.Name = "btnyolsec";
+            this.btnyolsec.Size = new System.Drawing.Size(107, 30);
+            this.btnyolsec.TabIndex = 7;
+            this.btnyolsec.Text = "Yol Seç";
+            this.btnyolsec.UseVisualStyleBackColor = true;
+            this.btnyolsec.Click += new System.EventHandler(this.btnyolsec_Click);
+            // 
+            // btnolustur
+            // 
+            this.btnolustur.Location = new System.Drawing.Point(466, 145);
+            this.btnolustur.Name = "btnolustur";
+            this.btnolustur.Size = new System.Drawing.Size(107, 30);
+            this.btnolustur.TabIndex = 8;
+            this.btnolustur.Text = "Oluştur";
+            this.btnolustur.UseVisualStyleBackColor = true;
+            this.btnolustur.Click += new System.EventHandler(this.btnolustur_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(95, 481);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(152, 40);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Metin Belgesi Oku";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(12, 199);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(306, 276);
+            this.listBox1.TabIndex = 10;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(493, 481);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(152, 40);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "Metin Belgesi Yaz";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(422, 199);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(303, 276);
+            this.richTextBox1.TabIndex = 12;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1177, 554);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.btnolustur);
+            this.Controls.Add(this.btnyolsec);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnyolsec;
+        private System.Windows.Forms.Button btnolustur;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ csharp_dosya-islemleri_my/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace csharp_dosya_islemleri_my
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.ShowDialog();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+        }
+
+
+        string dosyaadi, dosyayolu;
+
+        StreamWriter sw;
+
+        private void btnolustur_Click(object sender, EventArgs e)
+        {
+            dosyaadi = textBox2.Text;
+            sw = File.CreateText(dosyayolu + "\\" + dosyaadi + ".txt");
+            sw.Close();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog1.ShowDialog()==DialogResult.OK)
+            {
+                StreamReader sr=new StreamReader(openFileDialog1.FileName);
+                string satir = sr.ReadLine();
+                while(satir != null)
+                {
+                    listBox1.Items.Add(satir);
+                    satir = sr.ReadLine();
+                }
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Filter = "Metin Dosyaları|*.txt";
+            saveFileDialog1.Title = "Metin Belgesi Kayıt";
+            saveFileDialog1.ShowDialog();
+            StreamWriter sw = new StreamWriter(saveFileDialog1.FileName);
+            sw.WriteLine(richTextBox1.Text);
+            sw.Close();
+            MessageBox.Show("Metin Kaydedildi!");
+
+        }
+
+        private void btnyolsec_Click(object sender, EventArgs e)
+        {
+            if(folderBrowserDialog1.ShowDialog()==DialogResult.OK)
+            {
+                dosyayolu = folderBrowserDialog1.SelectedPath.ToString();
+                textBox1.Text = dosyayolu;
+            }
+        }
+
+
+    }
+}
